9a6b2ebc4952b48ccf211fe50dcba4bb33341b9f
Mitchell Paulus <paulusm14@gmail.com>
2023-12-11 11:10:14 -0600

    UI updates

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
+using Microsoft.CodeAnalysis;
 using ScottPlot;
 using ScottPlot.Avalonia;
 using ScottPlot.Plottables;
@@ -15,6 +16,32 @@ public class MainViewModel : INotifyPropertyChanged
     public ObservableCollection<DataSourceViewModel> Sources { get; } = new();
 
     public static AvaPlot Plot = new();
+
+    private string _trendFilter = "";
+    public string TrendFilter
+    {
+        get => _trendFilter;
+        set
+        {
+            if (SetField(ref _trendFilter, value))
+            {
+                foreach (var source in Sources)
+                {
+                    source.FilteredTrendBuffer.Clear();
+                    foreach (var trend in source.Trends)
+                    {
+                        if (trend.Name.Contains(_trendFilter))
+                        {
+                            source.FilteredTrendBuffer.Add(trend);
+                        }
+                    }
+
+                    source.UpdateFilteredTrends();
+                }
+            };
+        }
+
+    }
 
     public MainViewModel()
     {
@@ -60,18 +87,46 @@ public class MainViewModel : INotifyPropertyChanged
     }
 }
 
-public class DataSourceViewModel
+public class DataSourceViewModel : INotifyPropertyChanged
 {
     public IDataSource DataSource { get; }
 
     public string Header { get; set; }
     public ObservableCollection<TrendItemViewModel> Trends { get; }
 
+    public readonly ObservableCollection<TrendItemViewModel> FilteredTrendBuffer;
+
+    public void UpdateFilteredTrends()
+    {
+        OnPropertyChanged(nameof(FilteredTrends));
+    }
+
+    public ObservableCollection<TrendItemViewModel> FilteredTrends
+    {
+        get => FilteredTrendBuffer;
+    }
+
     public DataSourceViewModel(IDataSource dataSource)
     {
         DataSource = dataSource;
         Header = dataSource.Header;
         Trends = new ObservableCollection<TrendItemViewModel>(DataSource.Trends.Select(t => new TrendItemViewModel(t)));
+        FilteredTrendBuffer = new ObservableCollection<TrendItemViewModel>(DataSource.Trends.Select(t => new TrendItemViewModel(t)));
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
     }
 }
 
